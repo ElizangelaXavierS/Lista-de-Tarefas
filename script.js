2c4c5fed3642f29b01c7540089b9d1f0121f9f00
Elizangela Xavier <elizangelaxavier913@gmail.com>
2022-08-30 15:06:54 -0300

    Editado JavaScript

--- script.js
+++ script.js
@@ -3,15 +3,24 @@ const inputTarefa = document.getElementById("tarefa-nova");
 function adicionarTarefa(){
     const item = document.createElement("li");
 
-    item.innerHTML = `<li>
+  item.innerHTML = `<li>
     <span>${inputTarefa.value}</span>
     <button class="excluir">x</button>
   </li>`;
 
     const lista = document.querySelector("#lista");
     lista.appendChild(item);
+
+    inputTarefa.value = ""
 }
 
+const botaoAdicionar = document.querySelector("#btn-adicionar");
+botaoAdicionar.addEventListener("click", adicionarTarefa);
 
-const botao = document.querySelector("#btn-adicionar");
-botao.addEventListener("click", adicionarTarefa)
+function limparLista(){
+  const lista = document.querySelector("#lista");
+  lista.innerHTML = ""
+}
+
+const botaoLimpar = document.querySelector(".bt")
+botaoLimpar.addEventListener("click", limparLista);
