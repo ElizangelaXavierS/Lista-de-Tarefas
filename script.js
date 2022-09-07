2676878773ae752e3a3ff5b10999c17fc10417ab
Elizangela Xavier <elizangelaxavier913@gmail.com>
2022-09-07 11:48:10 -0300

    Estrutura JavaScript.

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
+let idTarefa = 0
 const inputTarefa = document.getElementById("tarefa-nova");
 
 function adicionarTarefa(){
     const item = document.createElement("li");
+    idTarefa++;
+    item.id = idTarefa
 
-  item.innerHTML = `<li>
-    <span>${inputTarefa.value}</span>
-    <button class="excluir">x</button>
-  </li>`;
-
-    const lista = document.querySelector("#lista");
+  item.innerHTML = `
+  <span onclick="concluir(${idTarefa})">${inputTarefa.value}</span>
+  <button class="excluir" onclick="excluir(${idTarefa})">x</button>
+  `;
+   const lista = document.querySelector("#lista");
     lista.appendChild(item);
 
     inputTarefa.value = ""
+    
+}
+
+function concluir(itemId) {
+  const tarefaSelecionada = document.getElementById(itemId);
+  tarefaSelecionada.className = "concluida";
 }
 
 const botaoAdicionar = document.querySelector("#btn-adicionar");
 botaoAdicionar.addEventListener("click", adicionarTarefa);
 
-function limparLista(){
-  const lista = document.querySelector("#lista");
-  lista.innerHTML = ""
-}
 
 const botaoLimpar = document.querySelector(".bt")
 botaoLimpar.addEventListener("click", limparLista);
+
+function limparLista(){
+  const lista = document.querySelector("#lista");
+  lista.innerHTML = ""
+  
+}function excluir (itemId){
+  const tarefaSelecionada = document.getElementById(itemId);
+  tarefaSelecionada.remove()
+}
+
+
